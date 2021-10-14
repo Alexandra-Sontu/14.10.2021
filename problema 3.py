d3a55764db6da3bdf67b745e86cfd98cd698fc7e
Alexandra-Sontu <71020688+Alexandra-Sontu@users.noreply.github.com>
2021-10-14 10:09:16 +0300

    Add files via upload

---
+++ problema 3.py
@@ -0,0 +1,17 @@
+print("Ce nume  si prenume ai")
+np=str(input("Introdu numele si prenumele"))
+print(np)
+print("Ce doresti sa devii in viitor")
+pr=str(input("Introdu o profesie"))
+print(pr)
+print("Te numesti",np)
+print("Vreau sa devin",pr)
+
+
+
+
+
+
+
+
+
